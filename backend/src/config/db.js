82fa5be143438395d40f0f76b48b23258e6882d4
VANSH PURI <vworkp625@gmail.com>
2026-01-05 01:21:01 +0530

    basic setup

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+const connectDB=async () => {
+  try{
+    if(!process.env.MONGO_URI){
+      throw new Error('MONGO_URI is not defined in environment variables')
+    }
+    await mongoose.connect(process.env.MONGO_URI,{
+      dbName:'ExpenseTracker'
+    })
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message)
+    process.exit(1)
+  }
+
+}
+export default connectDB; 
